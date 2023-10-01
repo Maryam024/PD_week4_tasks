6a6080cd25061c9cd348bcc50833ef98de9313a9
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-10-01 12:30:59 +0500

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void Reverse(string a);
+
+main()
+{
+ string a;
+ cout<<"Enter 'true' or 'false': ";
+ cin>> a;
+ 
+ Reverse(a);
+}
+void Reverse(string a)
+{
+ if(a=="true"){
+     cout<<"false";
+             }else
+{    cout<<"true";
+}
+
+
+}
